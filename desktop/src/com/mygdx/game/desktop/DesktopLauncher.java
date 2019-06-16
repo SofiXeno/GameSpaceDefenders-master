5aa3efc7451a149ba4b3a32e66488fc235883912
Sofi Xeno <46944287+SofiXeno@users.noreply.github.com>
2019-06-16 20:44:27 +0300

    New features added 4

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,6 +3,7 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGame;
+//hhjhjh
 
 public class DesktopLauncher {
 	public static final int WIDTH = 800, HEIGHT = 900;
